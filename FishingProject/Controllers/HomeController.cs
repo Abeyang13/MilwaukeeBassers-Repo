93562d4849b907bae4a91e27d72260dff9f1a3c2
iaiir <abeyang13@gmail.com>
2019-10-03 09:10:52 -0500

    Completed TournamentTeamChart action and view.  Now working on the AverageFishTournamentChart.

--- FishingProject/Controllers/HomeController.cs
+++ FishingProject/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using FishingProject.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace FishingProject.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,24 @@ namespace FishingProject.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult TournamentTeamAmountChart()
+        {
+            var tournaments = db.Tournaments.ToList();
+            List<int> AllTeams = new List<int>();
+            var allTeams = new List<ReportView>();
+            foreach (Tournament tournament in tournaments)
+            {
+                var teams = db.TournamentTeams.Include(t => t.Team).Where(t => t.TournamentId == tournament.TournamentId);
+                AllTeams.Add(teams.Count());
+                allTeams.Add(new ReportView
+                {
+                    DimensionOne = tournament.TournamentName,
+                    Quantity = teams.Count()
+                }); ;
+            }
+            return View(allTeams);
         }
     }
 }
---
+++ FishingProject/Models/ReportView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FishingProject.Models
+{
+    public class ReportView
+    {
+        [Key]
+        public int ReportViewId { get; set; }
+        public string DimensionOne { get; set; }
+        public int Quantity { get; set; }
+    }
+}
